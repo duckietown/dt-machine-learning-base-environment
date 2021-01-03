9293891eea845ef21ff1a7f46ac4d58972f28e7f
frank-qcd-qk <frank_chude.qian@outlook.com>
2021-01-03 00:06:05 -0500

    fix dataclass bug

--- assets/amd64/install_py3.sh
+++ assets/amd64/install_py3.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# remove dataclass
+pip3 uninstall dataclass
 
 # Pytorch: 
 pip install torch==1.7.0+cu101 torchvision==0.8.1+cu101 torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
