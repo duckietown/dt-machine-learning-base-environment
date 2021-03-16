7cdc7831e51bd14b9632373815bd9a901ee863a5
Andrea F Daniele <afdaniele@ttic.edu>
2021-03-16 12:30:49 -0500

    Update install.sh

--- assets/arm64v8/install.sh
+++ assets/arm64v8/install.sh
@@ -4,7 +4,7 @@ set -e
 
 # download PyTorch
 echo "Downloading PyTorch v${PYTORCH_VERSION}..."
-PYTORCH_WHEEL_NAME="torch-${PYTORCH_VERSION}-cp38-cp38-linux_aarch64.whl"
+PYTORCH_WHEEL_NAME="torch-${PYTORCH_VERSION}.cuda.cudnn-cp38-cp38-linux_aarch64.whl"
 WHEEL_URL="https://duckietown-public-storage.s3.amazonaws.com/assets/python/wheels/${PYTORCH_WHEEL_NAME}"
 wget -q "${WHEEL_URL}" -O "/tmp/${PYTORCH_WHEEL_NAME}"
 # install PyTorch
