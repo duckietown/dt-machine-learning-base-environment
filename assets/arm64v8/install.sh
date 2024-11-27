dfdd59a9082f0f203c9e9423002218f8e9ee0c35
Liam Paull <paulll@iro.umontreal.ca>
2024-11-26 20:03:18 -0500

    trying to fix

--- assets/arm64v8/install.sh
+++ assets/arm64v8/install.sh
@@ -1,35 +1,42 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-# install CUDA deps
+# setup nvidia repo
+sudo apt-key adv --fetch-keys \
+    https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/arm64/3bf863cc.pub
+
+echo "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/arm64 /" \
+    > /etc/apt/sources.list.d/cuda.list
+
+sudo apt-key adv --fetch-keys \
+    https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/arm64/7fa2af80.pub
+
+echo "deb https://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/arm64 /" \
+    > /etc/apt/sources.list.d/nvidia-ml.list
+
+# install CUDA 10.2
 apt-get update
 apt-get install -y --no-install-recommends \
-    libopenblas-base \
-    libopenmpi-dev \
-    g++-8 \
-    unzip
+    cuda-cudart-$CUDA_PKG_VERSION \
+    cuda-compat-10-2 \
+    cuda-libraries-$CUDA_PKG_VERSION \
+    cuda-npp-$CUDA_PKG_VERSION \
+    cuda-nvtx-$CUDA_PKG_VERSION \
+    libcublas10=10.2.2.89-1 \
+    libnccl2=$NCCL_VERSION-1+cuda10.2 \
+    libcudnn8=$CUDNN_VERSION-1+cuda10.2
+apt-mark hold \
+    libnccl2 \
+    libcudnn8 \
+    cuda-compat-10-2
 rm -rf /var/lib/apt/lists/*
 
-# download TensorRT
-echo "Downloading TensorRT v${TENSORRT_VERSION}..."
-TENSORRT_WHEEL_NAME=tensorrt-${TENSORRT_VERSION}-cp38-cp38-linux_aarch64.whl
-TENSORRT_WHEEL_URL="https://duckietown-public-storage.s3.amazonaws.com/assets/python/wheels/${TENSORRT_WHEEL_NAME}"
-wget -q "${TENSORRT_WHEEL_URL}" -O "/tmp/${TENSORRT_WHEEL_NAME}"
-# install TensorRT
-echo "Installing TensorRT v${TENSORRT_VERSION}..."
-pip3 install "/tmp/${TENSORRT_WHEEL_NAME}"
-rm "/tmp/${TENSORRT_WHEEL_NAME}"
+# TODO Install Tensor RT here
+# >>>...
 
-# download PyCUDA
-echo "Downloading PyCUDA v${PYCUDA_VERSION}..."
-PYCUDA_WHEEL_NAME=pycuda-${PYCUDA_VERSION}-cp38-cp38-linux_aarch64.whl
-PYCUDA_WHEEL_URL="https://duckietown-public-storage.s3.amazonaws.com/assets/python/wheels/${PYCUDA_WHEEL_NAME}"
-wget -q "${PYCUDA_WHEEL_URL}" -O "/tmp/${PYCUDA_WHEEL_NAME}"
-# install PyCUDA
-echo "Installing PyCUDA v${PYCUDA_VERSION}..."
-pip3 install "/tmp/${PYCUDA_WHEEL_NAME}"
-rm "/tmp/${PYCUDA_WHEEL_NAME}"
+# Clean up
+rm -rf /usr/src/cudnn_samples_v8
 
 # clean
 pip3 uninstall -y dataclasses
